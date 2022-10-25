a16a54a3b36bc156a8f9cf9b2d90224d5feb6da2
22rkdmin <22rkdmin@naver.com>
2022-10-26 06:52:16 +0900

    swagger 사용

--- src/main/java/com/zerobase/weather/controller/DiaryController.java
+++ src/main/java/com/zerobase/weather/controller/DiaryController.java
@@ -3,6 +3,8 @@ package com.zerobase.weather.controller;
 import com.zerobase.weather.domain.Diary;
 import com.zerobase.weather.model.DiaryInput;
 import com.zerobase.weather.service.DiaryService;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
 import lombok.RequiredArgsConstructor;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -17,36 +19,41 @@ public class DiaryController {
     private final DiaryService diaryService;
 
 
+    @ApiOperation(value = "일기 텍스트와 날씨를 이용해서 DB에 일기 저장")// swagger 컨트롤러 설명
     @PostMapping("/create/diary")
-    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+    void createDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "yyyy-MM-dd") LocalDate date,
                      @RequestBody DiaryInput diaryInput){
 
         diaryService.createDiary(date, diaryInput.getText());
     }
 
 
+    @ApiOperation(value = "선택한 날짜의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diary")
-    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+    List<Diary> readDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "yyyy-MM-dd") LocalDate date){
 
         return diaryService.readDiary(date);
     }
 
+    @ApiOperation(value = "선택한 날짜 기간의 모든 일기 데이터를 가져옵니다.")
     @GetMapping("/read/diaries")
-    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate startDate,
-                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate endDate){
+    List<Diary> readDiaries(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "yyyy-MM-dd") LocalDate startDate,
+                            @RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "yyyy-MM-dd") LocalDate endDate){
 
         return diaryService.readDiaries(startDate, endDate);
     }
 
+    @ApiOperation(value = "선택한 날짜의 일기를 수정합니다.")
     @PutMapping("/update/diary")
-    void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date,
+    void updateDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "yyyy-MM-dd") LocalDate date,
                      @RequestBody DiaryInput diaryInput){
         System.out.println(diaryInput.getText());
         diaryService.updateDiary(date, diaryInput.getText());
     }
 
+    @ApiOperation(value = "선택한 날짜의 일기 데이터를 삭제합니다.")
     @DeleteMapping("/delete/diary")
-    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) LocalDate date){
+    void deleteDiary(@RequestParam @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @ApiParam(value = "yyyy-MM-dd") LocalDate date){
         diaryService.deleteDiary(date);
     }
 
